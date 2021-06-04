3039a4eb23284f5b793abf37c3174af7a6c118cd
BeomChang <fnzlvkdvkd@naver.com>
2021-06-05 00:10:01 +0900

    6088 완료

--- 13. [기초-종합] 6077~ 6091/6088.py
+++ 13. [기초-종합] 6077~ 6091/6088.py
@@ -15,14 +15,15 @@
 # 시작 값(a), 등차(d), 몇 번째인지를 나타내는 정수(n)가 입력될 때
 # n번째 수를 출력하는 프로그램을 만들어보자.
 
-a, d, n = input().split(' ')
-
+a, d, n = input().split()
 a = int(a)
 d = int(d)
 n = int(n)
 
-current = a
-for i in range(n - 1):
-    current = current + d
+if n == 1:
+    print(a)
+else:
+    for i in range(n - 1):
+        a = a + d
 
-print(current)
+print(a)
